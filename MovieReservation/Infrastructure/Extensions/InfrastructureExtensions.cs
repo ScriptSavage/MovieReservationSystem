ab53ae60a8038072ce11c40f0227266954ab07e6
MAX <maksymiliangaleziowski@gmail.com>
2026-05-11 17:53:41 +0200

    Add Infrastructure Extension

---
+++ MovieReservation/Infrastructure/Extensions/InfrastructureExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+using Domain.Abstractions;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions;
+
+public static class InfrastructureExtensions
+{
+    public static void AddInfrastructureLayer(this IServiceCollection services , IConfiguration configuration)
+    {
+        
+        string? connectionString = configuration.GetConnectionString("DefaultConnection");
+        
+        
+        services.AddDbContext<DatabaseContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+}
