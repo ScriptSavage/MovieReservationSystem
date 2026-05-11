54425fbde6d685ff8fe5f9d96cabdbedd1bab789
MAX <maksymiliangaleziowski@gmail.com>
2026-05-11 17:53:20 +0200

    Add Db Context

---
+++ MovieReservation/Infrastructure/Context/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class DatabaseContext : IdentityDbContext<ApplicationUser>
+{
+    public DatabaseContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected DatabaseContext()
+    {
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
