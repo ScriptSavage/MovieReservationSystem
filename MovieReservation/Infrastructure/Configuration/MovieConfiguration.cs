c271c1fc434f3229c85c114e51524daa7d3d5b93
MAX <maksymiliangaleziowski@gmail.com>
2026-05-11 17:54:22 +0200

    Add Movie Configuration

---
+++ MovieReservation/Infrastructure/Configuration/MovieConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configuration;
+
+public sealed class MovieConfiguration : IEntityTypeConfiguration<Movie>
+{
+    public void Configure(EntityTypeBuilder<Movie> builder)
+    {
+
+        builder.HasKey(e => e.MovieId);
+
+        
+        builder.Property(e => e.Title)
+            .IsRequired()
+            .HasMaxLength(255);
+        
+        builder.Property(e=>e.DurationInMinutes)
+            .IsRequired();
+
+        builder.Property(e => e.ReleaseYear)
+            .IsRequired();
+        
+        builder.Property(e=>e.MinimumAgeRecruitment)
+            .IsRequired();
+        
+        
+
+
+
+
+    }
+}
