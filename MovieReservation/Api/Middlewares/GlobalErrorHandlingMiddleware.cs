7b096a6316eb2acb4fb5a96fce6bbc1abccd3a47
MAX <maksymiliangaleziowski@gmail.com>
2026-05-11 17:53:08 +0200

    Add Error Handling Middleware

---
+++ MovieReservation/Api/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace Api.Middlewares;
+
+public class GlobalErrorHandlingMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next.Invoke(context);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Something went wrong");
+            context.Response.StatusCode = 500;
+        }
+    }
+}
