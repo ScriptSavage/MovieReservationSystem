991577b5d27868b0bbc33de07cc110915831c220
MAX <maksymiliangaleziowski@gmail.com>
2026-05-11 17:53:57 +0200

    Add Unit Of Work Abstract

---
+++ MovieReservation/Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Domain.Abstractions;
+
+public interface IUnitOfWork 
+{
+    Task SaveChangesAsync(CancellationToken cancellationToken);
+    Task<IDbTransaction> BeginTransactionAsync(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted);
+}
