041c7920b0fe41a12b70637fdcffb17ecc8463f1
maksymilian <maksymiliangaleziowski@gmail.com>
2026-05-17 14:10:53 +0200

    Added DatabaseContext

--- MovieReservation/Infrastructure/Context/DatabaseContext.cs
+++ MovieReservation/Infrastructure/Context/DatabaseContext.cs
@@ -1,4 +1,6 @@
-﻿using Domain.Entities.Identity;
+﻿using Domain.Entities;
+using Domain.Entities.Identity;
+using Infrastructure.Configuration;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,17 +8,32 @@ namespace Infrastructure.Context;
 
 public class DatabaseContext : IdentityDbContext<ApplicationUser>
 {
-    public DatabaseContext(DbContextOptions options) : base(options)
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
     {
     }
+    
 
-    protected DatabaseContext()
-    {
-    }
-
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Venue> Venues { get; set; }
+    public DbSet<Event> Events { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<RefreshToken?> RefreshTokens { get; set; }
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfiguration(new MovieConfiguration());
+        modelBuilder.ApplyConfiguration(new GenreConfiguration());
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new VenueConfiguration());
+        modelBuilder.ApplyConfiguration(new EventConfiguration());
+        modelBuilder.ApplyConfiguration(new ReservationConfiguration());
+        modelBuilder.ApplyConfiguration(new TicketConfiguration());
+        modelBuilder.ApplyConfiguration(new TicketTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new RefreshTokenConfiguration());
         base.OnModelCreating(modelBuilder);
     }
 }
