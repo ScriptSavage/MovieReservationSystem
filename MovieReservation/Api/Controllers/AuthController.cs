81a99f9f1607bc576c3624387efe00d0d64195cf
maksymilian <maksymiliangaleziowski@gmail.com>
2026-05-17 13:55:55 +0200

    Added Auth Controller

---
+++ MovieReservation/Api/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+using System.Security.Claims;
+using Application.Abstraction;
+using Application.Dto.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserDto.RegisterUserRequest request)
+    {
+        var result = await _authService.RegisterUser(request);
+
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return Ok("User has been registered");
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<LoginUserDto.Response>> Login([FromBody] LoginUserDto.Request request)
+    {
+        var response = await _authService.LoginAsync(request);
+
+        return Ok(response);
+    }
+
+    [HttpPost("refresh-token")]
+    public async Task<ActionResult<LoginUserDto.Response>> RefreshToken([FromBody] RefreshTokenDto.Request request)
+    {
+        var response = await _authService.RefreshTokenAsync(request);
+
+        return Ok(response);
+    }
+    
+    
+    [HttpGet("user-test")]
+    [Authorize(Roles = "User")]
+    public IActionResult Get()
+    {
+        return Ok("you are user");
+    }
+}
