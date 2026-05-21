30678675f17a1976e4cc7f39eb319a70e2eaaa47
maksymilian <maksymiliangaleziowski@gmail.com>
2026-05-21 22:15:52 +0200

    Added crud operations

--- MovieReservation/Application/Abstraction/IUserService.cs
+++ MovieReservation/Application/Abstraction/IUserService.cs
@@ -1,8 +1,18 @@
 using Application.Dto.Auth;
+using Application.Dto.Reservation;
+using Application.Dto.User;
 
 namespace Application.Abstraction;
 
 public interface IUserService
 {
     Task ChangePasswordAsync(string userId ,ChangePasswordDto dto);
+    
+    Task ChangeEmailAsync(string userId, ChangeEmailDto dto);
+    
+    Task DeleteAccountAsync(string userId);
+    
+    Task<UserDetailsDto> GetUserDetailsAsync(string userId);
+    
+    Task<List<ReservationDto>> GetAllReservationsAsync(string userId);
 }
