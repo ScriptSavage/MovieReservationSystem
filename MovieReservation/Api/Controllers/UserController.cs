b442a6aaa363615addf1ec0d5249c579df719310
maksymilian <maksymiliangaleziowski@gmail.com>
2026-05-21 22:12:03 +0200

    Add Crud operations for user

--- MovieReservation/Api/Controllers/UserController.cs
+++ MovieReservation/Api/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using Application.Abstraction;
 using Application.Dto.Auth;
+using Application.Dto.User;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,7 +18,7 @@ public class UserController : ControllerBase
         _userService = userService;
     }
 
-    [HttpPost("change-password")]
+    [HttpPatch("change-password")]
     [Authorize(Roles = "User")]
     public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordDto dto)
     {
@@ -31,5 +32,64 @@ public class UserController : ControllerBase
         await _userService.ChangePasswordAsync(userId, dto);
 
         return Ok("Password has been changed");
+    }
+
+
+    [HttpPatch("change-email")]
+    [Authorize(Roles = "User")]
+    public async Task<IActionResult> ChangeEmail([FromBody] ChangeEmailDto dto)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId is null)
+        {
+            return Unauthorized();
+        }
+
+        await _userService.ChangeEmailAsync(userId,dto);
+        return Ok("Email has been changed");
+    }
+
+    [HttpDelete]
+    [Authorize(Roles = "User")]
+    public async Task<IActionResult> DeleteAccount()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (userId is null)
+        {
+            return Unauthorized();
+        }
+      
+        await _userService.DeleteAccountAsync(userId);
+        
+        return Ok("Account has been deleted");
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "User")]
+    public async Task<IActionResult> GetAccount()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId is null)
+        { 
+            return Unauthorized();
+        }
+        var userDetails = await _userService.GetUserDetailsAsync(userId);
+        return Ok(userDetails);
+    }
+
+
+    [HttpGet("reservations")]
+    [Authorize(Roles = "User")]
+    public async Task<IActionResult> GetUserReservations()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (userId is null)
+        {
+            return Unauthorized();
+        }
+        var userReservations = await _userService.GetAllReservationsAsync(userId);
+        return Ok(userReservations);
     }
 }
