7504d832f22b4b07b0340cf3fdd18d9684494bd1
maksymilian <maksymiliangaleziowski@gmail.com>
2026-05-24 09:57:54 +0200

    Added GenreController

---
+++ MovieReservation/Api/Controllers/GenreController.cs
@@ -0,0 +1,47 @@
+using Application.Abstraction;
+using Application.Dto.Genre;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/genres")]
+[Authorize(Roles = "Admin")]
+public class GenreController : ControllerBase
+{
+    private readonly IGenreService _genreService;
+
+    public GenreController(IGenreService genreService)
+    {
+        _genreService = genreService;
+    }
+
+    [HttpGet("{id:long}")]
+    public async Task<IActionResult> GetGenre(long id)
+    {
+        var genre =  await _genreService.GetGenre(id);
+        return Ok(genre);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllGenres()
+    {
+        var gereList =  await _genreService.GetAllGenres();
+        return Ok(gereList);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddGenre([FromBody] GenreDto.Request genre)
+    {
+        await _genreService.AddGenre(genre);
+        return Ok("Genre added");
+    }
+
+    [HttpDelete("{id:long}")]
+    public async Task<IActionResult> DeleteGenre(long id)
+    {
+        await _genreService.RemoveGenre(id);
+        return Ok("Genre deleted");
+    }
+}
