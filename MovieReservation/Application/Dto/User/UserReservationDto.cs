4e5bfbbe56a4dd1ae5b2c03f5e900874ae3b4c34
MAX <maksymiliangaleziowski@gmail.com>
2026-05-25 17:32:22 +0200

    Added UserReservationDto

---
+++ MovieReservation/Application/Dto/User/UserReservationDto.cs
@@ -0,0 +1,12 @@
+﻿using Application.Dto.Reservation;
+
+namespace Application.Dto.User;
+
+public class UserReservationDto
+{
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public ICollection<ReservationDto> Reservations { get; set; } = [];
+}
