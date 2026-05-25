3c11a6778ec949bd1a435f61f698b98fcebfcc5f
MAX <maksymiliangaleziowski@gmail.com>
2026-05-25 17:31:30 +0200

    Added AdminController

---
+++ MovieReservation/Api/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Abstraction;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/")]
+public class AdminController  : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public AdminController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("users")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> GetUsersReservations([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+    {
+        var result = await _userService.GetAllUsersReservationsAsync(page, pageSize);
+        return Ok(result);
+    }
+}
