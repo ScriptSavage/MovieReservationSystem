044c5841519de0f11e04c184efe01e4e6001b91b
maksymilian <maksymiliangaleziowski@gmail.com>
2026-05-26 11:49:00 +0200

    Change EventDto body

--- MovieReservation/Application/Dto/Event/EventDto.cs
+++ MovieReservation/Application/Dto/Event/EventDto.cs
@@ -2,4 +2,4 @@ using Application.Dto.Venue;
 
 namespace Application.Dto.Event;
 
-public record EventDto(DateTime StartDate, DateTime EndDate, VenueDto Venue);
+public record EventDto(string EventName,DateTime StartDate, DateTime EndDate, VenueDto Venue);
