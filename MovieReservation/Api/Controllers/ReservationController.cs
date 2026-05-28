4323fbfbea055febfa27b3f57333607c793681c7
maksymilian <maksymiliangaleziowski@gmail.com>
2026-05-28 13:17:20 +0200

    feat: added ReservationController

---
+++ MovieReservation/Api/Controllers/ReservationController.cs
@@ -0,0 +1,25 @@
+using Application.Abstraction;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/reservations")]
+public class ReservationController : ControllerBase
+{
+    private readonly IReservationService _reservationService;
+
+    public ReservationController(IReservationService reservationService)
+    {
+        _reservationService = reservationService;
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> GetReservationsAsync([FromQuery]int page, [FromQuery]int pageSize)
+    {
+        var result = await _reservationService.GetReservations(page, pageSize);
+        return Ok(result);
+    }
+}
