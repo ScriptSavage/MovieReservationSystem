2970ff00d3d23dddb6f0c56fe03c90eddf0c147a
maksymilian <maksymiliangaleziowski@gmail.com>
2026-05-28 13:13:05 +0200

    feat: added crud operations

--- MovieReservation/Domain/Abstractions/IVenueRepository.cs
+++ MovieReservation/Domain/Abstractions/IVenueRepository.cs
@@ -5,4 +5,10 @@ namespace Domain.Abstractions;
 public interface IVenueRepository
 {
     Task AddNewVenue(Venue venue);
+    
+    Task<Venue> GetVenue(long id);
+    
+    Task<IEnumerable<Venue>> GetAllVenues();
+    
+    Task <bool> DoesVenueExist(long id);
 }
--- MovieReservation/Infrastructure/Repository/VenueRepository.cs
+++ MovieReservation/Infrastructure/Repository/VenueRepository.cs
@@ -1,6 +1,7 @@
 using Domain.Abstractions;
 using Domain.Entities;
 using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repository;
 
@@ -16,6 +17,22 @@ public class VenueRepository : IVenueRepository
     public async Task AddNewVenue(Venue venue)
     {
         await _context.Venues.AddAsync(venue);
-        await _context.SaveChangesAsync();
+    }
+
+    
+
+    public async Task<Venue> GetVenue(long id)
+    {
+        return  await _context.Venues.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Venue>> GetAllVenues()
+    {
+        return await _context.Venues.ToListAsync();
+    }
+
+    public async Task<bool> DoesVenueExist(long id)
+    {
+        return await _context.Venues.AnyAsync(e=>e.VenueId == id);
     }
 }
